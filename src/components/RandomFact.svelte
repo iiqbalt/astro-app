6466c68a860081caad4c50a509d277e211dc1dc9
Iqbal Taufiqurrochman <iqbal.taufiqurrochman@gmail.com>
2025-10-05 11:39:25 +0700

    feat : random fact

---
+++ src/components/RandomFact.svelte
@@ -0,0 +1,31 @@
+<script>
+    import axios from "axios";
+
+    let fact = $state({});
+
+    function getQuote() {
+        axios
+            .get("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en")
+            .then((res) => {
+                console.log("res", res);
+                fact = res.data
+            })
+            .catch((err) => {
+                console.log("err", err);
+            });
+    }
+
+    $effect(() => {
+        getQuote();
+    });
+</script>
+
+<div class="card">
+    <div class="card-body">
+        <div class="text-right">
+            <i class="fas fa-spinner"></i>
+        </div>
+        <div class="font-bold text-2xl">{fact?.text}</div>
+        <a href="{fact?.source_url}" target="_blank" class="font-normal text-sm text-gray-400 mt-2">{fact?.source_url}</a>
+    </div>
+</div>
